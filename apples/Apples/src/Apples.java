d3ebb37bf84b88c0cae137f6bbb0307e990dc061
ovrdrv3 <donaven.snowden@gmail.com>
2016-03-22 21:08:57 -0700

    new program

--- apples/Apples/src/Apples.java
+++ apples/Apples/src/Apples.java
@@ -2,13 +2,16 @@ import java.util.Random;
 
 public class Apples {
 	public static void main(String[] args) {
-			Random dice = new Random();
-			int number;
-			for(int counter = 1; counter <=10;counter++){
-				number = dice.nextInt(6);
-				System.out.println(number + " ");
-			}
-			
+		Random rand = new Random();
+		int freq[] = new int[7];
+		for(int counter=1;counter<1000;counter++){
+			++freq[1+rand.nextInt(6)];
+	}
+	System.out.println("Dice\t Number of rolls");
+	
+	for(int face = 1;face <freq.length;face++){
+			System.out.println(face+"\t"+freq[face]);
+	}	
 	}
 }
- 
+
