166a1a8a0b72b50aab7d5d66ff738c0bf79da98e
ovrdrv3 <donaven.snowden@gmail.com>
2016-03-24 08:31:51 -0700

    new lines

--- madlib/madlib/src/madLib/madLibMainClass.java
+++ madlib/madlib/src/madLib/madLibMainClass.java
@@ -1,10 +1,14 @@
 package madLib;
 
 public class madLibMainClass {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		String longText[] = new String[3];
+		 longText[0] = "This is the hardwired string array to be used as an example of a string array";
+		 longText[1] = "This is the second line of text that can be printed";
+		 longText[2] = "The text should be eventually imported from a text file and new index for each gap in the sentence";
+		 	for(int counter=0;counter<longText.length;counter++){
+		 		System.out.println(longText[counter]);
+		 	}	
+	}	
 }
+
