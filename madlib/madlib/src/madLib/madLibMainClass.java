d90f6a9db7ea04fcf1eebe2248d8fd4f065eef3e
ovrdrv3 <donaven.snowden@gmail.com>
2016-03-29 08:32:39 -0700

    Proof of concept

--- madlib/madlib/src/madLib/madLibMainClass.java
+++ madlib/madlib/src/madLib/madLibMainClass.java
@@ -1,13 +1,43 @@
 package madLib;
+import javax.swing.JOptionPane;
 
 public class madLibMainClass {
+	
+/*	public madLibMainClass(int num){
+		
+		
+		num = 9;
+	}
+*/	
+	
+
+	
 	public static void main(String[] args) {
-		String longText[] = new String[3];
-		 longText[0] = "This is the hardwired string array to be used as an example of a string array";
-		 longText[1] = "This is the second line of text that can be printed";
-		 longText[2] = "The text should be eventually imported from a text file and new index for each gap in the sentence";
+	    String shortText[] = new String[9];
+		shortText[0] = JOptionPane.showInputDialog("Enter an animal.");
+		shortText[1] = JOptionPane.showInputDialog("Enter a location.");
+		shortText[2] = JOptionPane.showInputDialog("Enter a small number.");
+		shortText[3] = JOptionPane.showInputDialog("Enter a color.");
+		shortText[4] = JOptionPane.showInputDialog("Enter a body part.");
+		shortText[5] = JOptionPane.showInputDialog("Enter an adjective.");
+		shortText[6] = JOptionPane.showInputDialog("Enter another body part.");
+		shortText[7] = JOptionPane.showInputDialog("Enter a location.");
+		shortText[8] = "";
+		 
+		 String longText[] = new String[9];
+		 longText[0] = "Help! I cannot find my ";
+		 longText[1] = " anywhere! The last time I saw him/her was at my ";
+		 longText[2] = " about ";
+		 longText[3] = " days ago. He has ";
+		 longText[4] = ", tiny ";	
+		 longText[5] = " and ";
+		 longText[6] = ", super gross ";			 
+		 longText[7] = ". If you see any sign of him/her, call me or find me at ";
+		 longText[8] = " from 9 a.m. to 5 p.m.";
+
+	 
 		 	for(int counter=0;counter<longText.length;counter++){
-		 		System.out.println(longText[counter]);
+		 		System.out.println(longText[counter]+shortText[counter]);
 		 	}	
 	}	
 
